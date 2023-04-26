86703c62e7b659e86316595c2236388a8a4a68ac
xSmoked <xsmoked@xSmokeds-MacBook-Pro.local>
2023-04-26 16:13:36 +0100

    Created follow with word, count, getters and setters and constructors and toString

---
+++ java/src/ie/tudublin/Follow.java
@@ -0,0 +1,30 @@
+package ie.tudublin;
+
+
+
+public class Follow {
+    private String word;
+    private int count;
+
+    public Follow(String word, int count) {
+        this.word = word;
+        this.count = count;
+    }
+    
+    public String toString() {
+        return word + "\t" + count;
+    }
+
+    public String getWord() {
+        return word;
+    }
+    public void setWord(String word) {
+        this.word = word;
+    }
+    public int getCount() {
+        return count;
+    }
+    public void setCount(int count) {
+        this.count = count;
+    } 
+}
